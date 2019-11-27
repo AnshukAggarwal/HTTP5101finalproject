d2ec742ef63fe44c8c1b49550c7a6d35432e49e3
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2019-11-27 07:46:23 -0500

    Add project files.

---
+++ http5101finalproject-N01399681/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="http5101finalproject_N01399681.Global" Language="C#" %>
---
+++ http5101finalproject-N01399681/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace http5101finalproject_N01399681
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
